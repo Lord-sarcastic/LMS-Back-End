e383e69120c89f72d054e4a09288f067f098d1be
Lord-sarcastic <adeoti.15.jude@gmail.com>
2021-12-01 12:20:51 +0100

    Chore: Updated gitignore and reworked settings conf

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -4,7 +4,7 @@ from .production import *
 LOCALS_EXIST = 'Local settings found, overriding production settings...'
 LOCALS_NOT_EXIST = 'Local settings not found, using production settings...'
 try:
-    from .local import *
+    from .local_settings import *
     print(LOCALS_EXIST)
 except ImportError:
     print(LOCALS_NOT_EXIST)
