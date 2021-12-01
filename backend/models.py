6d153ef79683b53368b3fbf95c13fa2988798563
Lord-sarcastic <adeoti.15.jude@gmail.com>
2021-12-01 11:35:10 +0100

    Chore: Changed uuid to be uneditable

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class TrackObjectStateMixin(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
